4a2ad484dad3a5fbfc6cfdba85295858e568d007
joelcapistrano <joelcapistrano@gmail.com>
2021-08-15 18:49:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ if platform == 'android':
     from android.permissions import request_permissions, Permission
     request_permissions([
         Permission.WRITE_EXTERNAL_STORAGE,
-        Permission.READ_EXTERNAL_STORAGE
+        Permission.READ_EXTERNAL_STORAGE,
+        Permission.INTERNET
     ])
 
 Builder.load_file('interface.kv')
